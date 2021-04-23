dea1e7bc574d6a8b4873215c9f4f04e940ed5704
Matt Windsor <mattwindsor91@gmail.com>
2021-04-23 21:07:06 +0100

    Factor out split drawing

--- src/ui.rs
+++ src/ui.rs
@@ -5,6 +5,9 @@ pub mod gfx;
 pub use error::{Error, Result};
 use sdl2::rect::Point;
 
+use self::gfx::Renderer;
+use crate::model::run;
+
 /// The UI core.
 pub struct Core {
     sdl: sdl2::Sdl,
@@ -35,11 +38,8 @@ impl Core {
 
         let mut r = gfx::Renderer { screen, font };
 
-        let mut tl = Point::new(10, 10);
-        for s in run.splits.iter() {
-            r.put_str(&s.name, tl)?;
-            tl = gfx::metrics::offset(tl, 0, 1);
-        }
+        self.draw_splits(run.splits.iter(), &mut r)?;
+
         r.present();
 
         let mut events = self.sdl.event_pump().map_err(Error::SdlInit)?;
@@ -59,5 +59,21 @@ impl Core {
         }
 
         Ok(())
+    }
+
+    fn draw_splits<'a>(
+        &self,
+        splits: impl IntoIterator<Item = &'a run::Split>,
+        r: &mut Renderer,
+    ) -> Result<()> {
+        for (num, s) in splits.into_iter().enumerate() {
+            self.draw_split(s, num, r)?;
+        }
+        Ok(())
+    }
+
+    fn draw_split(&self, split: &run::Split, num: usize, r: &mut Renderer) -> Result<()> {
+        let tl = Point::new(4, 4 + (16 * num as i32));
+        r.put_str(&split.name, tl)
     }
 }
