b1573e704b0174009de2263301934534ac0e2d5f
Matt Windsor <mattwindsor91@gmail.com>
2021-05-18 21:27:02 +0100

    Refactor and add tests to time functins

--- src/model/time/field.rs
+++ src/model/time/field.rs
@@ -3,7 +3,12 @@ use super::{
     error::{Error, Result},
     position::Position,
 };
-use std::{convert::TryInto, fmt, marker::PhantomData, str::FromStr};
+use std::{
+    convert::{TryFrom, TryInto},
+    fmt,
+    marker::PhantomData,
+    str::FromStr,
+};
 
 /// A field in a time struct.
 pub struct Field<P> {
@@ -17,10 +22,7 @@ pub struct Field<P> {
 
 impl<P> Clone for Field<P> {
     fn clone(&self) -> Self {
-        Self {
-            val: self.val,
-            phantom: PhantomData::default(),
-        }
+        Self::new(self.val)
     }
 }
 
@@ -45,6 +47,37 @@ impl<P> From<Field<P>> for u16 {
     }
 }
 
+impl<P: Position> TryFrom<u32> for Field<P> {
+    type Error = Error;
+
+    /// Tries to fit `val` into this field.
+    ///
+    /// ```
+    /// use std::convert::TryFrom;
+    /// use zombiesplit::model::time::{Field, Second};
+    ///
+    /// let f1 = Field::<Second>::try_from(4);
+    /// assert!(f1.is_ok(), "shouldn't have overflowed");
+    /// let f2 = Field::<Second>::try_from(64);
+    /// assert!(!f2.is_ok(), "should have overflowed");
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Returns `Error::FieldTooBig` if `val` is too large for the field.
+    fn try_from(val: u32) -> Result<Self> {
+        let (field, carry) = Self::new_with_carry(val);
+        if carry == 0 {
+            Ok(field)
+        } else {
+            Err(Error::FieldTooBig {
+                pos: P::name(),
+                val,
+            })
+        }
+    }
+}
+
 impl<P: Position> FromStr for Field<P> {
     type Err = Error;
 
@@ -59,22 +92,17 @@ impl<P: Position> FromStr for Field<P> {
                 pos: P::name(),
                 err,
             })?;
-        let (field, carry) = Self::new_with_carry(val);
-        if carry == 0 {
-            Ok(field)
-        } else {
-            Err(Error::FieldTooBig {
-                pos: P::name(),
-                val,
-            })
-        }
+        Self::try_from(val)
     }
 }
 
 impl<P> Field<P> {
     /// Creates a new field with the given value.
+    ///
+    /// This is not widely exposed to avoid the invariant (val is between 0 and
+    /// position maximum) being broken.
     #[must_use]
-    pub fn new(val: u16) -> Self {
+    fn new(val: u16) -> Self {
         Self {
             val,
             phantom: PhantomData::default(),
@@ -97,13 +125,26 @@ impl<P: Position> Field<P> {
 
     /// Fits as much of `val` as possible into a field, and returns the field
     /// and any carry.
+    ///
+    /// ```
+    /// use zombiesplit::model::time::{Field, Second};
+    /// let (field, carry) = Field::<Second>::new_with_carry(64);
+    /// assert_eq!(u16::from(field), 4, "should have taken 4 seconds");
+    /// assert_eq!(carry, 1, "should have carried over 1 minute")
+    /// ```
     #[must_use]
     pub fn new_with_carry(val: u32) -> (Self, u32) {
         let (carry, val) = div_rem(val, P::cap());
         (Self::new(val.try_into().unwrap_or_default()), carry)
     }
-
     /// Returns this field's value as milliseconds.
+    ///
+    /// ```
+    /// use zombiesplit::model::time::{Field, Second};
+    /// use std::convert::TryFrom;
+    /// let msec = Field::<Second>::try_from(20).unwrap().as_msecs();
+    /// assert_eq!(msec, 20_000, "20 secs = 20,000 msecs");
+    /// ```
     #[must_use]
     pub fn as_msecs(self) -> u32 {
         u32::from(self.val) * P::ms_offset()
