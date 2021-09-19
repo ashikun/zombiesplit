3c3a979b235afad248366ee83a76d5d03e9ed7ad
Matt Windsor <mattwindsor91@gmail.com>
2021-09-19 20:07:43 +0100

    Clean up and add tests for Position

--- src/ui/view/gfx/position.rs
+++ src/ui/view/gfx/position.rs
@@ -2,9 +2,15 @@
 use super::metrics::{sat_i32, Rect};
 
 /// A position specification, used for moving the renderer's plotter.
-#[derive(Clone, Copy, Debug)]
+///
+/// [Position]s consist of an [X] component and a [Y] component, each of which
+/// can be relative to one edge of the theoretical bounding rectangle, or
+/// to the current position of the plotter.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Position {
+    /// The specification for the X coordinate.
     pub x: X,
+    /// The specification for the Y coordinate.
     pub y: Y,
 }
 
@@ -73,11 +79,13 @@ impl Position {
 }
 
 /// An X position specification.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum X {
-    /// Delta on current position.
+    /// Relative to current position.
     Rel(i32),
+    /// Relative to the left side of the bounding rectangle.
     Left(i32),
+    /// Relative to the right side of the bounding rectangle.
     Right(i32),
 }
 
@@ -96,6 +104,17 @@ impl X {
     }
 
     /// Converts to a left position given the current X position and width.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zombiesplit::ui::view::gfx::position::X;
+    ///
+    /// assert_eq!(27, X::Rel(2).to_left(25, 50));
+    /// assert_eq!(10, X::Left(10).to_left(25, 50));
+    /// assert_eq!(48, X::Right(2).to_left(25, 50));
+    /// ```
+
     #[must_use]
     pub fn to_left(self, cur_x: i32, width: u32) -> i32 {
         match self {
@@ -107,13 +126,13 @@ impl X {
 }
 
 /// A Y position specification.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Y {
     /// Relative to current position.
     Rel(i32),
-    /// Relative to top.
+    /// Relative to the top side of the bounding rectangle.
     Top(i32),
-    /// Relative to bottom.
+    /// Relative to the bottom side of the bounding rectangle.
     Bottom(i32),
 }
 
@@ -132,6 +151,16 @@ impl Y {
     }
 
     /// Converts to a top position given the current Y position and height.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zombiesplit::ui::view::gfx::position::Y;
+    ///
+    /// assert_eq!(27, Y::Rel(2).to_top(25, 50));
+    /// assert_eq!(10, Y::Top(10).to_top(25, 50));
+    /// assert_eq!(48, Y::Bottom(2).to_top(25, 50));
+    /// ```
     #[must_use]
     pub fn to_top(self, cur_y: i32, height: u32) -> i32 {
         match self {
