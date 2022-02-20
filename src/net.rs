beda895a20b1256af70adf743e59fe01a67a8dfa
Matt Windsor <mattwindsor91@gmail.com>
2022-02-20 00:06:58 +0000

    Start laying groundwork for move to gRPC
    Import the imports, produce a protobuf file, etc.

--- src/net.rs
+++ src/net.rs
@@ -1,8 +1,11 @@
 /*! zombiesplit client/server netcode.
 
 zombiesplit uses a client/server separation whereby the client and server talk to each other using
-a length-delimited CBOR extrusion of the action (client to server) and event (server to client)
-enumerations in the attempt model.  The netcode is asynchronous and built using tokio. */
+a protocol defined in `proto`.  The netcode is asynchronous and built using tokio. */
+
+pub mod proto {
+    tonic::include_proto!("zombiesplit");
+}
 
 pub mod client;
 mod io;
